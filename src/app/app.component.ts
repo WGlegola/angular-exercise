79c83063620b1d8a5a97a04b1d6bf83a6574f4da
Wojciech Glegola <wglegola@gmail.com>
2022-06-01 11:41:36 +0200

    added routing sourcecode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { AccountService } from 'src/account.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  accounts: {name:string, status:string}[]=[];
-  constructor(private accountService: AccountService){}
-  ngOnInit(): void {
-    this.accounts = this.accountService.accounts;
-  }
+export class AppComponent {
 }
--- src/app/logging.service.ts
+++
@@ -1,5 +0,0 @@
-export class LoggingService{
-    logStatusChange(message:string){
-    console.log('A server status changed, new status: ' + message);
-    }
-}
